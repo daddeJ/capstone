25445036f0cd62db33b7e155fc6340e16620187d
daddej <ikharimgloria@gmail.com>
2025-09-02 16:38:18 +0800

    Add Pachyderm Category in Zoo Simulation

--- zoo/src/main/java/com/simulation/zoo/config/ServiceConfiguration.java
+++ zoo/src/main/java/com/simulation/zoo/config/ServiceConfiguration.java
@@ -2,7 +2,9 @@ package com.simulation.zoo.config;
 
 import com.simulation.animal.entities.Animal;
 import com.simulation.animal.services.implementations.AnimalServiceImpl;
+import com.simulation.animal.services.implementations.category.PachydermServiceImpl;
 import com.simulation.animal.services.interfaces.AnimalService;
+import com.simulation.animal.services.interfaces.category.PachydermService;
 import com.simulation.generics.implementations.CrudServiceImpl;
 import com.simulation.generics.interfaces.CrudService;
 import org.springframework.context.annotation.Bean;
@@ -10,14 +12,16 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ServiceConfiguration {
-
     @Bean
     public CrudService<Animal, Long> crudService() {
         return new CrudServiceImpl<>();
     }
-
     @Bean
     public AnimalService animalService(CrudService<Animal, Long> crudService) {
         return new AnimalServiceImpl(crudService);
+    }
+    @Bean
+    public PachydermService pachydermService(CrudService<Animal, Long> crudService) {
+        return new PachydermServiceImpl(crudService);
     }
 }
--- zoo/src/test/java/com/simulation/mocks/MockAnimalFactory.java
+++ zoo/src/test/java/com/simulation/mocks/MockAnimalFactory.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.simulation.animal.entities.Animal;
+import com.simulation.animal.entities.category.Pachyderm;
 
 public class MockAnimalFactory {
     public static List<Animal> getMockAnimals() {
@@ -71,5 +72,62 @@ public class MockAnimalFactory {
         animals.add(parrot);
 
         return animals;
+    }
+
+    public static List<Animal> getMockPacyderm() {
+        List<Animal> pachyderms = new ArrayList<>();
+
+        Pachyderm rhino1 = new Pachyderm();
+        rhino1.setId(1L);
+        rhino1.setAnimalId("A001");
+        rhino1.setName("Chunky Chucky");
+        rhino1.setCategory("Pachyderm");
+        rhino1.setSpecies("Rhinoceros");
+        rhino1.setWeight(2300);
+        rhino1.setHeight(1.7);
+
+        Pachyderm rhino2 = new Pachyderm();
+        rhino2.setId(2L);
+        rhino2.setAnimalId("A002");
+        rhino2.setName("Chewy Chucky");
+        rhino2.setCategory("Pachyderm");
+        rhino2.setSpecies("Rhinoceros");
+        rhino2.setWeight(2500);
+        rhino2.setHeight(1.8);
+
+        Pachyderm hippo1 = new Pachyderm();
+        hippo1.setId(3L);
+        hippo1.setAnimalId("A003");
+        hippo1.setName("Chubby Chucky");
+        hippo1.setCategory("Pachyderm");
+        hippo1.setSpecies("Hippopotamus");
+        hippo1.setWeight(1500);
+        hippo1.setHeight(1.5);
+
+        Pachyderm hippo2 = new Pachyderm();
+        hippo2.setId(4L);
+        hippo2.setAnimalId("A004");
+        hippo2.setName("Clumsy Chucky");
+        hippo2.setCategory("Pachyderm");
+        hippo2.setSpecies("Hippopotamus");
+        hippo2.setWeight(1700);
+        hippo2.setHeight(1.6);
+
+        Pachyderm elephant = new Pachyderm();
+        elephant.setId(5L);
+        elephant.setAnimalId("A005");
+        elephant.setName("Chaotic Chucky");
+        elephant.setCategory("Pachyderm");
+        elephant.setSpecies("Elephant");
+        elephant.setWeight(5000);
+        elephant.setHeight(3.2);
+
+        pachyderms.add(rhino1);
+        pachyderms.add(rhino2);
+        pachyderms.add(hippo1);
+        pachyderms.add(hippo2);
+        pachyderms.add(elephant);
+
+        return  pachyderms;
     }
 }
